27bd662123137cbec1d32f57da2ddaa208882e11
jedrek0429 <64475360+jedrek0429@users.noreply.github.com>
2024-03-29 13:05:40 +0100

    Remove redundant newlines

--- core/src/main/java/net/pl3x/map/core/configuration/Lang.java
+++ core/src/main/java/net/pl3x/map/core/configuration/Lang.java
@@ -204,10 +204,8 @@ public final class Lang extends AbstractConfig {
     public static String UI_BLOCKINFO_LABEL = "BlockInfo";
     @Key("ui.blockinfo.value")
     public static String UI_BLOCKINFO_VALUE = "<block><br /><biome>";
-    
     @Key("ui.blockinfo.unknown.block")
     public static String UI_BLOCKINFO_UNKNOWN_BLOCK = "Unknown block";
-    
     @Key("ui.blockinfo.unknown.biome")
     public static String UI_BLOCKINFO_UNKNOWN_BIOME = "Unknown biome";
     @Key("ui.coords.label")
