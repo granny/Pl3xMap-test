eff01b792d4af66da336a6d389ae766e6c06b84e
granny <contact@granny.dev>
2024-04-05 11:56:05 -0700

    content -> context

--- webmap/src/control/ContextMenuControl.ts
+++ webmap/src/control/ContextMenuControl.ts
@@ -6,7 +6,7 @@ import {insertCss, removeCss} from "../util/Util";
 export default class ContextMenuControl extends L.Control {
     private readonly _pl3xmap: Pl3xMap;
     private _dom: HTMLDivElement = L.DomUtil.create('div');
-    private _id: string = 'pl3xmap-contentmenu';
+    private _id: string = 'pl3xmap-contextmenu';
 
 
     constructor(pl3xmap: Pl3xMap) {
